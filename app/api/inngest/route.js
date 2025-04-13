21023c14095417839d16c4503820e33b4ca91d3f
samraddhi-singh <2k22.csai.2211502@gmail.com>
2025-04-13 12:00:36 +0530

    preparing for deployment

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    generateIndustryInsights
   ],
 });
